745a48d8ada9962aa21eea333121a5c12e8f72f1
bonjon <pica.1816394@studenti.uniroma1.it>
2023-09-10 23:35:50 +0200

    SI

--- gossipy/model/handler.py
+++ gossipy/model/handler.py
@@ -312,6 +312,9 @@ class TorchModelHandler(ModelHandler):
             # We select directly to_keep clients from the dict_params2.
             # After that we perform Multi-Krum, we select the one that minimize the distance.
             best_indices = closest_indices[:self.to_keep]
+            print("best_indices: ", best_indices)
+            print(len(dicts_params2))
+            print(dicts_params2)
             best_results = [dicts_params2[i] for i in best_indices]
             print("best_results: ", best_results)
             dict_params1 = self._fedavg(dict_params1, best_results)
